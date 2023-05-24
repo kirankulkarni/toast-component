bb63cf6748b9c9d6bf0faaff440740b6a51ee22a
Kiran Kulkarni <kk.questworld@gmail.com>
2023-05-24 14:26:04 +0530

    Add solution for exercise 1
    - Add a state to control the message
    - Render Varient Options
    - Add CSS to disable border for fieldset
    - Capture varient radio state in a state

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -7,44 +7,61 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState('');
+  const [varient, setVarient] = React.useState(VARIANT_OPTIONS[0]);
+
+  console.log(varient);
   return (
     <div className={styles.wrapper}>
       <header>
-        <img alt="Cute toast mascot" src="/toast.png" />
+        <img alt='Cute toast mascot' src='/toast.png' />
         <h1>Toast Playground</h1>
       </header>
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
-            htmlFor="message"
+            htmlFor='message'
             className={styles.label}
             style={{ alignSelf: 'baseline' }}
           >
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              id='message'
+              className={styles.messageInput}
+              value={message}
+              onChange={(event) => {
+                setMessage(event.target.value);
+              }}
+            />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
-          </div>
+          <fieldset>
+            <div
+              className={`${styles.inputWrapper} ${styles.radioWrapper}`}
+            >
+              {VARIANT_OPTIONS.map((option) => (
+                <label key={option} htmlFor={`varient-${option}`}>
+                  <input
+                    id={`varient-${option}`}
+                    type='radio'
+                    name='variant'
+                    value={option}
+                    checked={varient === option}
+                    onChange={(event) => {
+                      setVarient(event.target.value);
+                    }}
+                  />
+                  {option}
+                </label>
+              ))}
+            </div>
+          </fieldset>
         </div>
 
         <div className={styles.row}>
